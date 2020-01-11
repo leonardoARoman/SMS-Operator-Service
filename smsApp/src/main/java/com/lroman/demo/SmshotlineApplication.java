cbcf26b57e67c4e505318236eef50dfd83262a33
leonardoARoman <leoroman@LeoRomans-Air.home>
2020-01-11 13:49:42 -0500

    spring boot project created

---
+++ smsApp/src/main/java/com/lroman/demo/SmshotlineApplication.java
@@ -0,0 +1,13 @@
+package com.lroman.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmshotlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmshotlineApplication.class, args);
+	}
+
+}
